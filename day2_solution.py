b2ec63891656e9b3b3564b3ff40f1d9c000e278f
david mckelvie <69944671+mckelvied@users.noreply.github.com>
2025-05-22 14:51:29 -0700

    Create day2_solution.py

---
+++ day2_solution.py
@@ -0,0 +1,58 @@
+import os
+import requests
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from IPython.display import Markdown, display
+import ollama
+
+# Constants
+OLLAMA_API = "http://localhost:11434/api/chat"
+HEADERS = {"Content-Type": "application/json"}
+MODEL = "llama3.2"
+
+
+class Website:
+
+    def __init__(self, url):
+        """
+        Create this Website object from the given url using the BeautifulSoup library
+        """
+        self.url = url
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.title = soup.title.string if soup.title else "No title found"
+        for irrelevant in soup.body(["script", "style", "img", "input"]):
+            irrelevant.decompose()
+        self.text = soup.body.get_text(separator="\n", strip=True)
+
+# A function that writes a User Prompt that asks for summaries of websites:
+def user_prompt_for(website):
+    user_prompt = f"You are looking at a website titled {website.title}"
+    user_prompt += "\nThe contents of this website is as follows; \
+please provide a short summary of this website in markdown. \
+If it includes news or announcements, then summarize these too.\n\n"
+    user_prompt += website.text
+    return user_prompt
+
+def messages_for(website):
+    return [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt_for(website)}
+    ]
+
+def summarize(url):
+    website = Website(url)
+    payload = {
+        "model": MODEL,
+        "messages": messages_for(website),
+        "stream": False
+    }
+    response = requests.post(OLLAMA_API, json=payload, headers=HEADERS)
+    print(response.json()['message']['content'])
+    
+
+system_prompt = "You are an assistant that analyzes the contents of a website \
+and provides a short summary, ignoring text that might be navigation related. \
+Respond in markdown."
+
+summarize("https://www.bbc.com/news")
